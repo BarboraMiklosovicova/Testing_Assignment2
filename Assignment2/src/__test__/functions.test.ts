12e00ebd762c222afbbddee07ba2fbdaa6fe8a3b
Home <home@MacBook-Air-som-tillhor-Barbora.local>
2022-12-05 04:09:36 +0100

    final changes

--- Assignment2/src/__test__/functions.test.ts
+++ Assignment2/src/__test__/functions.test.ts
@@ -1,35 +1,26 @@
 import { movieSort } from "../ts/functions";
 import { IMovie } from "../ts/models/Movie";
-import { mockData } from "../ts/services/__mocks__/movieservice"; // import the mockdata to enable sorting based on array-objects
+import { mockData } from "../ts/services/__mocks__/movieservice"; 
 
 test ("should sort the array from descending a-z", () => {
-    //arrange
-
+    //ARRANGE
     let movies: IMovie[] = mockData;
 
-    //act
+    //ACT
     movieSort(movies);
 
-    //assert
+    //ASSERT
     expect(movies[0].Title).toBe("Harry Potter 1");
 });
 
-/*test("should sort the array that contains 2 objects with equal keyvalue-pair for title", () => {
-
-    //arrange
-    let movies:IMovie[] = mockData;
-    //act
-    movieSort(movies,false);
-    //assert
-    expect(movies[3].Title).toEqual(movies[4].Title);
-
-});*/
 
 test ("should sort the array from ascending z-a", () => {
-    //arrange
+    //ARRANGE
     let movies: IMovie[] = mockData;
-    //act
-    movieSort(movies, false); // false since ascending
-    //assert
+    
+    //ACT
+    movieSort(movies, false); 
+    
+    //ASSERT
     expect(movies[3 & 4].Title).toBe("Harry Potter 3");
 });
--- Assignment2/src/__test__/movieApp.test.ts
+++ Assignment2/src/__test__/movieApp.test.ts
@@ -12,7 +12,7 @@ jest.mock("./../ts/services/movieservice.ts");
 
 describe ("init-function", () => {
   test("should run handlesubmit function when button clicked.", () => {
-    // arr
+    // ARRANGE
     expect.assertions(1);
     let one = jest.spyOn(fnMovieApp, "handleSubmit").mockImplementation(() =>
     new Promise((resolve) => {
@@ -24,9 +24,11 @@ describe ("init-function", () => {
     <button type="submit" id="search">Sök</button>
     </form>`;
     fnMovieApp.init();
-    //act
+    
+    //ACT
     document.getElementById("search")?.click();
-    //assert
+    
+    //ASSERT
     expect(one).toHaveBeenCalled();
     document.body.innerHTML = "";
   });
@@ -39,7 +41,7 @@ describe ("handleSubmit - if/else", () => {
   });
 
   test("should run the createhtml-function", async () => {
-    //Arrange
+    //ARRANGE
     expect.assertions(1);
     document.body.innerHTML = `<form id="searchForm">
     <input type="text" id="searchText" value="Harry" placeholder="Skriv titel här" />
@@ -47,128 +49,40 @@ describe ("handleSubmit - if/else", () => {
     </form>
     <div id="movie-container"></div>`;
     let one = jest.spyOn(fnMovieApp, "createHtml").mockReturnValue();
-    //Act
+    
+    //ACT
     await fnMovieApp.handleSubmit();
-    //Assert
+    
+    //ASSERT
     expect(one).toHaveBeenCalled();
     document.body.innerHTML = "";
   });
 
-  /*test("should run displayNoResult, catch", async () => {
-    //arr
-    expect.assertions(1);
-    document.body.innerHTML = `<form id="searchForm">
-    <input type="text" id="searchText" value="" placeholder="Skriv titel här" />
-    <button type="submit" id="search">Sök</button>
-    </form>
-    <div id="movie-container"></div>`;
-    let one = jest.spyOn(fnMovieApp,"displayNoResult").mockReturnValue();
-    //act
-    await fnMovieApp.handleSubmit();
-    //ass
-    expect(one).toHaveBeenCalled();
-    //document.body.innerHTML = "";
-  });*/
 
   test("Should display a message", () => {
-    //Arrange
+    //ARRANGE
     let container: HTMLDivElement = document.createElement("div") as HTMLDivElement;
-    //Act
+    
+    //ACT
     fnMovieApp.displayNoResult(container);
-    //Assert
+    
+    //ASSERT
     expect(container.innerHTML).toBe(`<p>Inga sökresultat att visa</p>`);
   });
 
 });
 
 test("should createhtml", async () => {
-  //arrange
-  document.body.innerHTML = `<div id="movie-container"></div>`; // container for the movies that are to be displayed in one div/movie-container
-  let container: HTMLDivElement = document.getElementById("movie-container") as HTMLDivElement; // the containerdiv
+  //ARRANGE
+  document.body.innerHTML = `<div id="movie-container"></div>`; 
+  let container: HTMLDivElement = document.getElementById("movie-container") as HTMLDivElement;
   let searchText: string = "Harry";
-  let movies = await movieServiceFunctions.getData(searchText); //////////////////////////////7
-  //act
+  let movies = await movieServiceFunctions.getData(searchText);
+  
+  //ACT
   await fnMovieApp.createHtml(movies, container);
-  // assert
+  
+  // ASSERT
   expect(document.querySelectorAll("div.movie").length).toBe(4);
 });
 
-
-
-
-
-
-
-
-
-
-
-/*/*test("should be able to call function handleSubmit", () => {
-  //ARRANGE
-  let spy = jest.spyOn(fnMovieApp, "handleSubmit").mockReturnThis();
-  document.body.innerHTML = `<form id="searchForm">>button type="sumbit" id="search">Sök>/button></form>`;
-  
-  fnMovieApp.init();
-
-  //ACT
-  (document.getElementById("searchForm") as HTMLFormElement)?.submit();
-
-  //ASSERT
-  expect(spy).toHaveBeenCalled();
-
-});
-
-
-test("should be able to submit form by clicking button", () => {
-  //Arrange
-  let spySubmit = jest.spyOn(fnMovieApp, "handleSubmit").mockReturnThis();
-  document.body.innerHTML = `<form id=“searchForm”>
-  <input type=“text” id=“searchText” placeholder=“Skriv titel här” />
-  <button type=“submit” id=“search”>Sök</button>
-  </form>`;                         //let form = (document.querySelector("searchForm") as HTMLFormElement);
-  
-  //Act
-  document.querySelector("button")?.click();
-  
-  //Assert
-  expect(spySubmit).toHaveBeenCalledTimes(1);
-});
-
-/*Bidrar inte med mer coverage!
-test("should be able to submit form through click", () => {
-  //Arrange
-  let spy = jest.spyOn(movieAppFunctions, "handleSubmit").mockReturnThis();
-  document.body.innerHTML = `<form id="searchForm"><button type="submit" id="search">Sök</button></form>`;
-  
-  //Act
-  document.querySelector("button")?.click();
-  
-  //Assert
-  expect(spy).toHaveBeenCalled();
-});*/
-
-/*test("Should create HTML", async () => {
-  //Arrange
-  document.body.innerHTML = `<div id="movie-container"></div>`;
-  let container: HTMLDivElement = document.getElementById("movie-container") as HTMLDivElement;
-  let searchText: string = "Harry";
-  let movies = await getData(searchText);
-  
-  //Act
-  fnMovieApp.createHtml(movies, container);
-  
-  //Assert
-  expect(document.querySelectorAll("div.movie").length).toBe(3);
-});
-
-test("Should display a message", () => {
-  //Arrange
-  document.body.innerHTML = "";
-  let container: HTMLDivElement = document.createElement("div") as HTMLDivElement;
-  
-  //Act
-  fnMovieApp.displayNoResult(container);
-  
-  //Assert
-  expect(container.innerHTML).toBe(`<p>Inga sökresultat att visa</p>`);
-});*/
